cad071259cd491cf9e46bc570c2419eee247a5e6
filipeparedes <202300257@estudantes.ips.pt>
2024-04-04 10:20:04 +0100

    Nivel 1

--- AirTransportation.java
+++ AirTransportation.java
@@ -1,2 +1,15 @@
 public class AirTransportation extends Transport{
+
+    String name;
+    int numberOfContainers;
+
+    public AirTransportation(String name, int numberOfContainers)
+    {
+        this.name = name;
+        this.numberOfContainers = numberOfContainers;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -1,2 +1,10 @@
 public class GroundTransportation extends Transport{
+
+    String licensePlate;
+
+    public GroundTransportation(String licensePlate)
+    {
+        this.licensePlate = licensePlate;
+    }
 }
+
